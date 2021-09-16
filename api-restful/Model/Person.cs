a61644cf3ae9f335c2f2c970e1754255314ce66a
Victor Luiz <victorluiz@MacBook-de-Victor.local>
2021-09-15 22:59:44 -0300

    Implementation interface

--- api-restful/Model/Person.cs
+++ api-restful/Model/Person.cs
@@ -4,11 +4,14 @@ namespace api_restful.Controllers.Model
 {
     public class Person
     {
-
         public long Id { get; set; }
+
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+
         public string Address { get; set; }
+
         public string Gender { get; set; }
     }
 }
---
+++ api-restful/Services/Implementations/IPersonService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using api_restful.Controllers.Model;
+
+namespace api_restful.Services.Implementations
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person FindById(long id);
+
+        Person Update(Person person);
+
+        void Delet(long id);
+
+        List<Person> FindAll();
+    }
+}
