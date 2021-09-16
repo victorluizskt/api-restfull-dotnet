d691fb0446b437773fbaf20c74e2c856e742e592
Victor Luiz <victorluiz@MacBook-de-Victor.local>
2021-09-15 22:54:59 -0300

    Working with four verbs

---
+++ api-restful/Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        #region 
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(
+            [FromRoute] string firstNumber,
+            [FromRoute] string secondNumber
+        )
+        {
+           
+
+            return BadRequest("Invalid input");
+        }
+        #endregion
+    }
+}
